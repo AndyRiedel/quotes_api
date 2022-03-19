591c2f692548225c89ed7a475ae49ef0b6e55375
Charles Riedel <das_stig@yahoo.com>
2022-03-18 23:51:47 -0500

    modify for heroku

--- config/Database.php
+++ config/Database.php
@@ -1,16 +1,15 @@
 <?php
     class Database {
         //db params
-        private $host = 'localhost';
-        private $db_name = 'quotesdb';
-        private $username = 'root';
+        private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'gajoltsx73w8pkr8';
+        private $username = 'f33o65ql7jm9mzit';
         private $password = '';
         private $conn;
 
         //password
         public function __construct(){
-            //$this->password = getenv('JAWSDB_PW', false);
-            
+            $this->password = getenv('JAWSDB_PW', false);
         }
 
         //db connect
